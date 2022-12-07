d4f4205ac6488b8cdd7d286e613437a41dbafcd3
0wx <xvnyan@gmail.com>
2022-12-07 13:28:38 +0700

    menambahkan data sementara kuis TTS

---
+++ data.js
@@ -0,0 +1,52 @@
+const tts = [
+  {
+    pertanyaan: 'Matahari tenggelam di sebelah…',
+    jawaban: 'Gawat',
+    komentar: 'Gawat kalau matahari tenggelam di sebelah kita.',
+  },
+  {
+    pertanyaan: 'Galang menyimpan buku di…',
+    jawaban: 'Mas',
+    komentar: 'Bukunya dimas dipinjam galang kemarin sore.',
+  },
+  {
+    pertanyaan: 'Bersatu kita teguh bercerai kita——-',
+    jawaban: 'Sedih',
+    komentar: 'Siapa yang gak sedih cerai sama istri?',
+  },
+  {
+    pertanyaan: 'Makanan yang dibungkus daun pisang?',
+    jawaban: 'Lontong',
+    komentar: 'Gampang kan? suka ga sama lontong?',
+  },
+  {
+    pertanyaan: 'Apabila mengendarai mobil wajib bawa',
+    jawaban: 'Satu',
+    komentar: 'Kalo bawa 2 gimana mengendarainya',
+  },
+  {
+    pertanyaan: 'Mawar melati semuanya…',
+    jawaban: 'Bunga',
+    komentar: 'Kalo bukan bunga apa coba, hewan?',
+  },
+  {
+    pertanyaan: 'Jangan membuang tisu di…',
+    jawaban: 'Tempatnya',
+    komentar: 'Masih di tempatnya kok di buang.',
+  },
+  {
+    pertanyaan: 'Ketika _____ orang biasanya makan.',
+    jawaban: 'Marah',
+    komentar: 'Orang marah apa enggak ya pasti makan.',
+  },
+  {
+    pertanyaan: 'Burung adalah hewan yang bisa..',
+    jawaban: 'Temenan',
+    komentar: 'Kan kalo terbang rame-rame kan sama temennya.',
+  },
+  {
+    pertanyaan: 'Hewan yang suka buka mulut lebar',
+    jawaban: 'Kenaflu',
+    komentar: 'Itu karena ga bisa napas idungnya mampet.',
+  },
+]
